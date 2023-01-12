bb5238094532e699f5f2db905cda05ce707847ff
mikzanco <mikyzancolo97@gmail.com>
2023-01-12 17:47:36 +0100

    create page show and detail, import logo e fatto che mi riporti alla home se cliccato

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,10 +5,14 @@
     <div class="container py-5">
         <h1>{{$comic->title}}</h1>
 
+        <div>Serie: <strong>{{$comic->series}}</strong></div>
+        <div>Data: <strong>{{$comic->sale_date}}</strong></div>
+        <div>Prezzo: <strong>{{$comic->price}}</strong></div>
+
         <div class="text-center py-5">
             <img src="{{$comic->image}}" alt="{{$comic->title}}">
         </div>
-
+        <p>{!!$comic->description!!}</p>
 
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,5 @@
-<header>Header</header>
+<header class="text-center py-5">
+    <a href="{{route('home')}}">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="Logo">
+    </a>
+</header>

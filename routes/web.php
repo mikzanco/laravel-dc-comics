5506993cea6fc50b7415e1ab6f76ce1fa7c2f6fe
mikzanco <mikyzancolo97@gmail.com>
2023-01-12 16:34:24 +0100

    create route resource in web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// tutte le rotte avranno come base comics
+Route::resource('comics', ComicController::class);
+// php artisan route:list

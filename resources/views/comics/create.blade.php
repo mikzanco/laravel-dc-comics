1eccbacada81f4badb41966153d9d1ad9976d390
mikzanco <mikyzancolo97@gmail.com>
2023-01-13 16:28:07 +0100

    add invalid in input fiel per segnare un errore maggiore nei campi specifici

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -19,27 +19,57 @@
             @csrf
             <div class="mb-3">
                 <label for="titolo" class="form-label">Titolo *</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{old('title')}}" placeholder="Scrivi il Titolo">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" value="{{old('title')}}" placeholder="Scrivi il Titolo">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Immagine *</label>
-                <input type="text" class="form-control" name="image" id="image" value="{{old('image')}}" placeholder="Scrivi la URL">
+                <input type="text" class="form-control @error('image') is-invalid @enderror" name="image" id="image" value="{{old('image')}}" placeholder="Scrivi la URL">
+                @error('image')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo *</label>
-                <input type="text" class="form-control" name="price" id="price" value="{{old('price')}}" placeholder="Scrivi il prezzo">
+                <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" id="price" value="{{old('price')}}" placeholder="Scrivi il prezzo">
+                @error('price')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie *</label>
-                <input type="text" class="form-control" name="series" id="series" value="{{old('series')}}" placeholder="Scrivi la serie">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" id="series" value="{{old('series')}}" placeholder="Scrivi la serie">
+                @error('series')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Giorno vendite *</label>
-                <input type="text" class="form-control" name="sale_date" id="sale_date" value="{{old('sale_date')}}" placeholder="Scrivi il giorno di inizio vendita">
+                <input type="text" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" id="sale_date" value="{{old('sale_date')}}" placeholder="Scrivi il giorno di inizio vendita">
+                @error('sale_date')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Genere *</label>
-                <input type="text" class="form-control" name="type" id="type" value="{{old('type')}}" placeholder="Scrivi il Genere">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type" value="{{old('type')}}" placeholder="Scrivi il Genere">
+                @error('type')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione *</label>
